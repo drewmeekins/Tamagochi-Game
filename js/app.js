acaaa4e54f3a262b375f0f5646bc8981cf6911db
dmeekins <drew.a.meekins@gmail.com>
2022-07-08 13:40:11 -0400

    setting up class

---
+++ js/app.js
@@ -0,0 +1,12 @@
+class Tamagotchi {
+    constructor(name, age){
+        this.name = name
+        this.age = age
+        this.hunger = 10
+        this.sleepiness = 10
+        this.boredom = 10
+    }
+
+    
+
+}
