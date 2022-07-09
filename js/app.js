30c31a64cf1032044d145339afb660d579ece413
drewmeekins <drew.a.meekins@gmail.com>
2022-07-09 11:26:29 -0400

    getting more functions to work

--- js/app.js
+++ js/app.js
@@ -25,7 +25,35 @@ class Tamagotchi {
 
 }
 
+const startButton = document.querySelector(`#start`)
+const eatButton = document.querySelector(`#eat`)
+const playButton = document.querySelector(`#play`)
+const lightsButton = document.querySelector(`#lights`)
+const name = document.querySelector(`.name`)
+
+const link = new Tamagotchi(`${name}`)
+
 // game
-const game = () => {
-    const hunger()
+const game = {
+    setHunger() {
+        const intervalID = setInterval(() => {
+            const hungerID = document.querySelector(`.hunger`)
+        })
+    },
+
+    startGame() {
+        alert(`Welcome to the The Tamagatchi Tournament`)
+    },
+
+    setName() {
+        this.name = prompt(`What's your Tamagotchi's name?`)
+        const nameID = document.querySelector(`.name`)
+        nameID.innerText = `${this.name}`
+    }
 }
+
+startButton.addEventListener('click', () => {
+    game.startGame()
+    game.setName()
+    console.log('working start')
+})
